8e463a9d45645406fa80ca4be08c8a522fc5687c
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-28 20:12:24 -0500

    updated

--- Storm/Jenkinsfile.groovy
+++ Storm/Jenkinsfile.groovy
@@ -22,6 +22,6 @@ node {
     sh "ssh ec2-user@${khuslen} sudo pip install -r /home/ec2-user/stormpath-flask-sample/requirements.txt"
   }
   stage("Run App"){
-    sh "ssh ec2-user@${khuslen} sudo python /home/ec2-user/stormpath-flask-sample/bootstrap.py"
+    sh "ssh ec2-user@${khuslen} sudo python /home/ec2-user/stormpath-flask-sample/app.py"
   }
 }
