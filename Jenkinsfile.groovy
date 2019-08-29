c8d13fa272cc74760ab7568e980e341d79484b1a
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-28 19:18:39 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,7 +10,7 @@ node {
     sh "ssh ec2-user@${khuslen} sudo rm -rf /home/ec2-user/flask-examples"
   }
   stage("Pull Repo"){
-    sh "ssh ec2-user@${khuslen} git clone https://github.com/miguelgrinberg/flask-examples.git"
+    sh "ssh ec2-user@${khuslen} git clone https://github.com/Khuslentuguldur/flask-examples.git"
   }
   stage("Install requirements"){
     // sh "virtualenv /tmp/venv"
@@ -19,9 +19,9 @@ node {
 
   }
   stage("Pip install"){
-    sh "ssh ec2-user@${khuslen} sudo pip install -r ~/flask-examples/requirements.txt"
+    sh "ssh ec2-user@${khuslen} sudo pip install -r /home/ec2-user/flask-examples/requirements.txt"
   }
   stage("Run App"){
-    sh "ssh ec2-user@${khuslen} sudo python ~/flask-examples/01-hello-world/hello.py"
+    sh "ssh ec2-user@${khuslen} sudo python /home/ec2-user/flask-examples/01-hello-world/hello.py"
   }
 }
